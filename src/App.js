052537dcf9229065e4b4e243af4474890668b570
Manishshriwas <manishshriwasjnvmahoba@gmail.com>
2025-08-17 17:48:11 +0530

    about with dark mode update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import About from './components/About.jsx';
 import Navbar from './components/Navbar.jsx';
 import TextForm from './components/TextForm.jsx';
 
@@ -10,6 +11,10 @@ function App() {
       <Navbar title='TextChanger' aboutText="About Us"/>
       <div className="container">
         <TextForm heading="Enter the text to analyze below"/>
+      
+      </div>
+      <div className='container'>
+      <About/>
       </div>
 
    </>
